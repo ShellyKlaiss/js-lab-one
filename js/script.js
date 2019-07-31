3084b937f84ebde3983276a052cf94b38537afbe
shelly <shellynacole@yahoo.com>
2019-07-31 16:43:40 -0400

    updated

--- js/script.js
+++ js/script.js
@@ -1,15 +1,15 @@
-var  text = prompt ("Who has the longest name Madison, Grant, or Bill?");
+var  text = prompt ("Identify the longest of the three names: Madison, Grant, or Bill?");
 const name1 = 'Madison';
 const name2 = 'Grant';
 const name3 = 'Bill';
 
 
 if (text === name1) {
-console.log ( "CORRECT! Madison is the longest name.");
+console.log ( "Madison is the longest name.");
 }else if (text === name2) {
-    console.log ("FALSE: Madison is a longer name than Grant.");
+    console.log ("Madison is a longer name than Grant.");
 } else if (text === name3){
-    console.log ("FALSE: Madison is a longer name than Bill.");
+    console.log ("Madison is a longer name than Bill.");
 } else {
     console.log ("Please enter Madison, Grant, or Bill");
 }
